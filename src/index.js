7265094407e1cfb462b4d0e64e849be4641e3e5a
avinash514 <avinashbabu.dimmeta@gmail.com>
2022-09-20 11:46:25 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,49 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
-
-
-
 const targetRepoName = core.getInput('repo-name');
 const ghToken = core.getInput('org-admin-token');
 const templateRepoName = core.getInput('template-repo-name');
+const targetOwner = core.getInput('owner');
+const description = core.getInput('description');
+const include_all_branches = core.getInput('include_all_branches');
+const private = core.getInput('private');
 const octokit = github.getOctokit(ghToken);
-
 const targetOrgName = github.context.payload.repository.owner.login;
 
-  
-/*const response = await octokit.request('POST /repos/{template_owner}/{template_repo}/generate', {
-    template_owner: targetOrgName,
-    template_repo: templateRepoName,
-    owner: targetOrgName,
-    name: targetRepoName,
-    description: 'This is your first repository',
-    include_all_branches: false,
-    'private': false
-  })*/
-
-  // await octokit.rest.repos.createUsingTemplate({
-  //   template_owner: targetOrgName,
-  //   template_repo: templateRepoName,
-  //   name: targetRepoName,
-  // });
-
-/*(async function run(){
-  const response = await octokit.rest.repos.createUsingTemplate({
-    template_owner: targetOrgName,
-    template_repo: templateRepoName,
-    name: targetRepoName,
-  });
-  console.log(response);
-})();*/
 
 async function run() {
   const response = await octokit.rest.repos.createUsingTemplate({
     template_owner: targetOrgName,
     template_repo: templateRepoName,
-    owner: 'avinash514org',
+    owner: targetOwner,
     name: targetRepoName,
-    include_all_branches: true,
+    description: description,
+    include_all_branches: include_all_branches,
+    private: private,
   });
   //console.log(response)
   console.log("Repo "+response.data.name+' created successfully!');
